676b43f83a9b8c6d38b6ed59f8ea27f4cb884947
ISXM77 <haloallstar117@hotmail.com>
2025-10-06 14:04:30 -0700

    test

---
+++ Lecture_Demo/game_stats.cpp
@@ -0,0 +1,8 @@
+// ISSA MARTINEZ
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Detroit Lions at Baltimore Ravens" << endl
+    << "Sept 29, 2025 Game Statistics" << endl;
+    return 0;
+}

47ec2343ccc2717ca12164ed27ad2d9c986cc48b
ISXM77 <haloallstar117@hotmail.com>
2025-10-06 14:14:43 -0700

    add function

--- game_stats.cpp
+++ game_stats.cpp
@@ -1,6 +1,7 @@
 // ISSA MARTINEZ
 #include <iostream>
 using namespace std;
+void showDrive();
 int main() {
     const int SIZE = 10;
     cout << "Detroit Lions at Baltimore Ravens" << endl
@@ -8,5 +9,17 @@ int main() {
     int yards[SIZE] = { 67, 98, 35, 25, 45, 50, 30, 40 };
     cout << "First drive yards: " << yards[0] << endl;
     cout << "Epic second drive: " << yards[1] << " yards" << endl;
+    showDrive();
+    showDrive();
+
+    cout << "Array memeory: ~" << sizeof(yards) << " bytes" << endl;
+    
     return 0;
+}
+void showDrive() {
+
+    static int driveNum = 1;
+    cout << "Showing drive " << driveNum << endl;
+    driveNum++;
+
 }

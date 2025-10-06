8a55a513e2c8551ef3fc66073cbed3c4190c9b4a
ISXM77 <haloallstar117@hotmail.com>
2025-10-06 14:58:17 -0700

    comment out and use struct

--- game_stats.cpp
+++ game_stats.cpp
@@ -2,14 +2,20 @@
 #include <iostream>
 using namespace std;
 void showDrive();
+void showYards(int arr[], int size);
+struct Drive {
+    int yards;
+    int plays;
+};
 int main() {
     const int SIZE = 10;
+    Drive drives[SIZE] = { {67,11}, {98, 18}, {60, 10}, {94, 12}, {35, 5}, {25, 4}, {45, 6}, {50,7}, {30, 5}, {40, 6} };
     cout << "Detroit Lions at Baltimore Ravens" << endl
     << "Sept 29, 2025 Game Statistics" << endl;
-    int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
-    cout << "First drive yards: " << yards[0] << endl;
-    cout << "Epic second drive: " << yards[1] << " yards" << endl;
-
+   //  int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
+    cout << "First drive yards: " << drives[0].yards << endl;
+    cout << "Epic second drive: " << drives[1].yards << " yards" << endl;
+    /*
     for (int i = 0; i < SIZE; i++) {
         cout << "Drive " << i + 1 << ": " << yards[i] << " yards\n";
     }
@@ -37,12 +43,16 @@ int main() {
             maxYards = yards[i];
     }
     cout << "Longest drive: " << maxYards << " yards" << endl;
-
+    */
     int plays[SIZE] = { 11, 18, 10, 12, 5, 4, 6, 7, 5, 6 };
     for (int i = 0; i < SIZE; i++) {
-        cout << "Drive " << i + 1 << ": " << plays[i] << " plays, "
-            << yards[i] << " yards" << endl;
+        cout << "Drive " << i + 1 << ": " << drives[i].plays << " plays, "
+            << drives[i].yards << " yards" << endl;
     }
+    /*cout << "Yards via function: ";
+    showYards(yards, SIZE);
+    cout << endl;
+    */
     return 0;
 }
 void showDrive() {
@@ -50,5 +60,10 @@ void showDrive() {
     static int driveNum = 1;
     cout << "Showing drive " << driveNum << endl;
     driveNum++;
+
+}
+void showYards(int arr[], int size) {
+    for (int i = 0; i < size; i++) cout << arr[i] << " ";
+    cout << endl;
 
 }
